82014dc1eaca79901d317f350153c3f9039c18f7
KimSehyeong <lego5542@naver.com>
2023-10-20 09:01:14 +0900

    tdd test

--- src/main/java/tdd/FizzBuzz.java
+++ src/main/java/tdd/FizzBuzz.java
@@ -2,9 +2,24 @@ package tdd;
 
 public class FizzBuzz {
 
+    // if number is divisible by 3 , print Fizz
+    // if number is divisible by 5 , print Buzz
+    // if number is divisible by 3 and 5 , print FizzBuzz
+    // if number is NOT divisible by 3 or 5 , then print he number
 
     public static String computer(int i) {
+//        if(i == 3){
+//            return "Fizz";
+//        }
 
-        return "";
+        if ((i % 3 == 0) && (i % 5 == 0)){
+            return "FizzBuzz";
+        }else if(i % 3  == 0){
+            return "Fizz";
+        }else if ( i % 5 == 0){
+            return "Buzz";
+        }else {
+            return "1";
+        }
     }
 }
--- src/test/java/tdd/FizzBuzzTestTest.java
+++ src/test/java/tdd/FizzBuzzTestTest.java
@@ -7,11 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FizzBuzzTest {
 
-    // if number is divisible by 3 , print Fizz
-    // if number is divisible by 5 , print Buzz
-    // if number is divisible by 3 and 5 , print FizzBuzz
-    // if number is NOT divisible by 3 or 5 , then print he number
 
+    // if number is divisible by 3 , print Fizz
     @Test
     @DisplayName("Disable by 3 ")
     @Order(1)
@@ -20,4 +17,30 @@ class FizzBuzzTest {
         assertEquals(expected, FizzBuzz.computer(3), "Fizz가 return 되어야한다");
     }
 
+    // if number is divisible by 5 , print Buzz
+    @Test
+    @DisplayName("Disable by 5 ")
+    @Order(2)
+    void testForDIvisibleBy5(){
+        String expected = "Buzz";
+        assertEquals(expected, FizzBuzz.computer(5), "Buzz return 되어야한다");
+    }
+
+    // if number is divisible by 3 and 5 , print FizzBuzz
+    @Test
+    @DisplayName("Disable by 3 and 5 ")
+    @Order(3)
+    void testForDIvisibleBy3and5(){
+        String expected = "FizzBuzz";
+        assertEquals(expected, FizzBuzz.computer(30), "FizzBuzz return 되어야한다");
+    }
+
+    // if number is NOT divisible by 3 or 5 , then print he number
+    @Test
+    @DisplayName("Not Disable by 3 and 5 ")
+    @Order(4)
+    void testForNOTDIvisibleBy3and5(){
+        String expected = "1";
+        assertEquals(expected, FizzBuzz.computer(1), "1이 되어야한다");
+    }
 }
